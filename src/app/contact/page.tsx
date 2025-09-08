9cf8d01e7255789b7d09d44009bbd0198e32002d
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2025-09-08 19:06:12 +0600

    'chamge-contact-for-small-and-large-device'

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -43,10 +43,12 @@ export default function Contact() {
 				<h1 className="capitalize text-2xl font-bold text-center mb-3">
 					get in touch now 😊, let&apos;s have a cup of tea ☕
 				</h1>
-				<div className="grid grid-cols-2 md:grid-cols-3 items-center py-10">
-					<nav>
+
+				{/* Contact Info */}
+				<div className="grid grid-cols-2 md:grid-cols-3 items-center justify-center py-10">
+					<div>
 						<Image
-							className="w-15 rounded-full p-4 bg-gradient-to-r from-[#0c8d8dbc] to-[#7b1]"
+							className="w-15 mx-auto md:mx-0 rounded-full p-4 bg-gradient-to-r from-[#0c8d8dbc] to-[#7b1] mb-3 md:mb-0"
 							src={location}
 							alt="address"
 						/>
@@ -54,30 +56,31 @@ export default function Contact() {
 						<address className="link link-hover">
 							<span>Manikganj,Dhaka</span>
 						</address>
-					</nav>
-					<nav>
+					</div>
+					<div>
 						<Image
-							className="w-15 rounded-full p-4 bg-gradient-to-r from-[#0c8d8dbc] to-[#7b1]"
+							className="w-15 mx-auto md:mx-0 rounded-full p-4 bg-gradient-to-r from-[#0c8d8dbc] to-[#7b1] mb-3 md:mb-0"
 							src={phone}
 							alt="phone"
 						/>
 						<h6 className="footer-title">Phone</h6>
 						<p>01628860956</p>
 						<p>01998304128</p>
-					</nav>
-					<nav>
+					</div>
+					<div className="col-span-2 md:col-span-1">
 						<Image
-							className="w-15 rounded-full p-4 bg-gradient-to-r from-[#0c8d8dbc] to-[#7b1]"
+							className="w-15 mx-auto md:mx-0 rounded-full p-4 bg-gradient-to-r from-[#0c8d8dbc] to-[#7b1] mb-3 md:mb-0"
 							src={mail}
 							alt="mail"
 						/>
 						<h6 className="footer-title">Email</h6>
 						<a className="link link-hover">abdullahalmuhammad6@gmail.com</a>
-						<a className="link link-hover">abdullahalmuhammad6@gmail.com</a>
-					</nav>
+					</div>
 				</div>
+
+				{/* Social */}
 				<div className="grid grid-cols-1 md:grid-cols-3 gap-5">
-					<div>
+					<div className="mx-auto md:mx-0">
 						<h2 className="capitalize text-2xl mb-2">let&apos;s connect</h2>
 						<a
 							className="link link-hover flex items-center gap-2"
@@ -101,17 +104,19 @@ export default function Contact() {
 							<p className="capitalize">github</p>
 						</a>
 					</div>
+
+					{/* Contact Form */}
 					<div>
-						<h2 className="text-2xl mb-3">Send me a message</h2>
+						<h2 className="text-2xl font-semibold mb-4">Send Me a Message</h2>
 						<form
 							ref={form}
 							onSubmit={sendEmail}
-							className="grid grid-cols-1 md:grid-cols-2 gap-2"
+							className="grid grid-cols-1 md:grid-cols-2 gap-4"
 						>
 							<div>
-								<label className="block">Name</label>
+								<label className="block mb-1">Name</label>
 								<input
-									className="input"
+									className="input w-full border px-3 py-2 rounded"
 									type="text"
 									name="user_name"
 									placeholder="Your Name"
@@ -119,26 +124,31 @@ export default function Contact() {
 								/>
 							</div>
 							<div>
-								<label>Email</label>
+								<label className="block mb-1">Email</label>
 								<input
-									className="input"
+									className="input w-full border px-3 py-2 rounded"
 									type="email"
 									name="user_email"
+									placeholder="Your Email"
 									required
 								/>
 							</div>
-							<div className="col-span-2">
-								<label>Message</label>
+							<div className="col-span-1 md:col-span-2">
+								<label className="block mb-1">Message</label>
 								<textarea
-									className="textarea row-span-2 block"
+									className="textarea w-full border px-3 py-2 rounded"
 									name="message"
+									placeholder="Your Message"
+									required
+								></textarea>
+							</div>
+							<div className="col-span-1 md:col-span-2 flex justify-center">
+								<input
+									className="btn w-32 bg-gradient-to-r from-[#0c8d8dbc] to-[#7b1] text-white px-4 py-2 rounded cursor-pointer"
+									type="submit"
+									value="Send"
 								/>
 							</div>
-							<input
-								className="btn w-24 bg-gradient-to-r from-[#0c8d8dbc] to-[#7b1]"
-								type="submit"
-								value="Send"
-							/>
 						</form>
 					</div>
 				</div>
