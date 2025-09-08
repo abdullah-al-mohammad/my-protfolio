604d1160b333aeab53652b07d75a59203b1cd66b
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2025-09-08 16:16:07 +0600

    'add-logo'

--- src/app/navbar/page.tsx
+++ src/app/navbar/page.tsx
@@ -1,5 +1,7 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import logo from "../../../public/favicon.ico";
 
 const Navbar = () => {
 	const navLinks = (
@@ -44,33 +46,14 @@ const Navbar = () => {
 							{navLinks}
 						</ul>
 					</div>
-					<a className="btn btn-ghost text-xl">abdullah</a>
+					<a className="btn btn-ghost text-xl">
+						<Image className="w-20" src={logo} alt="ab"></Image>
+						abdullah
+					</a>
 				</div>
 				<div className="navbar-center hidden lg:flex">
-					<ul className="menu menu-horizontal px-1">
-						{/* <li>
-							<a>Item 1</a>
-						</li>
-						<li>
-							<details>
-								<summary>Parent</summary>
-								<ul className="p-2">
-									<li>
-										<a>Submenu 1</a>
-									</li>
-									<li>
-										<a>Submenu 2</a>
-									</li>
-								</ul>
-							</details>
-						</li>
-						<li>
-							<a>Item 3</a>
-						</li> */}
-						{navLinks}
-					</ul>
+					<ul className="menu menu-horizontal px-1">{navLinks}</ul>
 				</div>
-				<div className="navbar-end">{/* <a className="btn">Button</a> */}</div>
 			</div>
 		</div>
 	);
