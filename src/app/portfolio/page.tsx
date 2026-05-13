80ed448de35cd85fee6a04d249a2293a5816db67
abdullah-al-mohammad <abdullahalmuhammad6@gmai.com>
2026-05-13 07:11:49 +0600

    implement portfolio project showcase page with image assets

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -13,6 +13,7 @@ import 'swiper/css/pagination';
 import { Autoplay } from 'swiper/modules';
 import image1 from '../../../public/images/Blood Donation.png';
 import image3 from '../../../public/images/ecommerce.jpg';
+import image4 from '../../../public/images/logo.png';
 import image2 from '../../../public/images/studentLession.jpg';
 
 import Image from 'next/image';
@@ -52,19 +53,19 @@ export default function Portfolio() {
         >
           <SwiperSlide className="pb-20">
             <div className="card bg-base-100 mb-5 md:mb-0 shadow-2xl">
-              <Image className="h-48 w-full object-cover" src={image1} alt="blood" />
+              <Image className="h-48 w-full object-cover" src={image4} alt="blood" />
               <div className="card-body p-4">
                 <h2 className="card-title text-2xl font-bold h-13 overflow-hidden group-hover:text-amber-400 transition-all duration-500">
                   NexBuy
                 </h2>
                 <p className="font-bold h-5 overflow-hidden">NexBuy — Modern eCommerce Store</p>
                 <p className=" h-5 overflow-hidden">
-                  <span className="font-bold">Technologies:</span> NexBuy is a modern eCommerce It
-                  offers fast performance, secure authentication, responsive design, product
-                  management, cart, and online shopping features.
+                  NexBuy is a modern eCommerce It offers fast performance, secure authentication,
+                  responsive design, product management, cart, and online shopping features.
                 </p>
                 <p className="line-clamp-4">
-                  website built with Next.js, TypeScript, MongoDB, and Mongoose.
+                  <span className="font-bold">Technologies:</span> Next.js, TypeScript, MongoDB,
+                  Mongoose.
                 </p>
                 <div className="card-actions mt-auto justify-end">
                   <a
